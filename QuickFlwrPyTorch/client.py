6341ba1db909f98a1c1b7dddbe633b2e91c280ec
aintburak <aintburak@gmail.com>
2024-04-09 20:39:19 +0300

    main: Add load_data method

---
+++ QuickFlwrPyTorch/client.py
@@ -0,0 +1,24 @@
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
+
+import flwr as fl
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def load_data():
+    """Load CIFAR-10 (training and test set)."""
+    transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+    trainset = CIFAR10(".", train=True, download=True, transform=transform)
+    testset = CIFAR10(".", train=False, download=True, transform=transform)
+    trainloader = DataLoader(trainset, batch_size=32, shuffle=True)
+    testloader = DataLoader(testset, batch_size=32)
+    num_examples = {"trainset" : len(trainset), "testset" : len(testset)}
+    return trainloader, testloader, num_examples
