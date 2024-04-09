317231284ee9b8632855705aa86b49b633a3df68
aintburak <aintburak@gmail.com>
2024-04-09 16:11:59 +0300

    main: Add FlowerClient

--- imageClassificationFL/Client.py
+++ imageClassificationFL/Client.py
@@ -134,3 +134,27 @@ def set_parameters(net, parameters: List[np.ndarray]):
 def get_parameters(net) -> List[np.ndarray]:
     return [val.cpu().numpy() for _, val in net.state_dict().items()]
 
+
+class FlowerClient(fl.client.NumPyClient):
+    def __init__(self, net, trainloader, valloader):
+        self.net = net
+        self.trainloader = trainloader
+        self.valloader = valloader
+
+    def get_parameters(self, config):
+        return get_parameters(self.net)
+
+    def fit(self, parameters, config):
+        set_parameters(self.net, parameters)
+        train(self.net, self.trainloader, epochs=1)
+        return get_parameters(self.net), len(self.trainloader), {}
+
+    def evaluate(self, parameters, config):
+        set_parameters(self.net, parameters)
+        loss, accuracy = test(self.net, self.valloader)
+        return float(loss), len(self.valloader), {"accuracy": float(accuracy)}
+
+
+
+
+
