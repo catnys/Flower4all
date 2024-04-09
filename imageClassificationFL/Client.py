9630b9650f6f4fe14a57172e2aaae8d62662b436
aintburak <aintburak@gmail.com>
2024-04-09 11:19:30 +0300

    main: Add load_datasets method

--- imageClassificationFL/Client.py
+++ imageClassificationFL/Client.py
@@ -13,9 +13,46 @@ from torch.utils.data import DataLoader
 import flwr as fl
 from flwr.common import Metrics
 from flwr_datasets import FederatedDataset
-
+"""
 DEVICE = torch.device("cpu")  # Try "cuda" to train on GPU
 print(
     f"Training on {DEVICE} using PyTorch {torch.__version__} and Flower {fl.__version__}"
 )
 disable_progress_bar()
+"""
+
+NUM_CLIENTS = 10
+BATCH_SIZE = 32
+
+
+def load_datasets():
+    fds = FederatedDataset(dataset="cifar10", partitioners={"train": NUM_CLIENTS})
+
+    def apply_transforms(batch):
+        # Instead of passing transforms to CIFAR10(..., transform=transform)
+        # we will use this function to dataset.with_transform(apply_transforms)
+        # The transforms object is exactly the same
+        transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+        batch["img"] = [transform(img) for img in batch["img"]]
+        return batch
+
+    # Create train/val for each partition and wrap it into DataLoader
+    trainloaders = []
+    valloaders = []
+    for partition_id in range(NUM_CLIENTS):
+        partition = fds.load_partition(partition_id, "train")
+        partition = partition.with_transform(apply_transforms)
+        partition = partition.train_test_split(train_size=0.8, seed=42)
+        trainloaders.append(DataLoader(partition["train"], batch_size=BATCH_SIZE))
+        valloaders.append(DataLoader(partition["test"], batch_size=BATCH_SIZE))
+    testset = fds.load_split("test").with_transform(apply_transforms)
+    testloader = DataLoader(testset, batch_size=BATCH_SIZE)
+    return trainloaders, valloaders, testloader
+
+
+trainloaders, valloaders, testloader = load_datasets()
