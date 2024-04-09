94c01f393149bdcb0ba797670f86aba41220dfbf
aintburak <aintburak@gmail.com>
2024-04-09 18:12:49 +0300

    main: Adjust Server.py

--- imageClassificationFL/Server.py
+++ imageClassificationFL/Server.py
@@ -1,33 +1,3 @@
-from typing import Tuple, List
-
 import flwr as fl
 
-from Client import client_fn
-
-NUM_CLIENTS = 10  # Number of clients participating in federated learning
-client_resources = {"cpu": 1}  # Define resources for each client, for example
-
-# Define metric aggregation function
-def weighted_average(metrics: List[Tuple[int, fl.common.Metrics]]) -> fl.common.Metrics:
-    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
-    examples = [num_examples for num_examples, _ in metrics]
-    return {"accuracy": sum(accuracies) / sum(examples)}
-
-# Create FedAvg strategy
-strategy = fl.server.strategy.FedAvg(
-    fraction_fit=1.0,
-    fraction_evaluate=0.5,
-    min_fit_clients=10,
-    min_evaluate_clients=5,
-    min_available_clients=10,
-    evaluate_metrics_aggregation_fn=weighted_average,
-)
-
-# Start simulation
-fl.simulation.start_simulation(
-    client_fn=client_fn,
-    num_clients=NUM_CLIENTS,
-    config=fl.server.ServerConfig(num_rounds=5),
-    strategy=strategy,
-    client_resources=client_resources,
-)
+fl.server.start_server(config=fl.server.ServerConfig(num_rounds=3))
