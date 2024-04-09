140e19a298186ab947d6739a7f6464da50950681
aintburak <aintburak@gmail.com>
2024-04-09 15:35:29 +0300

    main: Add test,train and Net

--- imageClassificationFL/Client.py
+++ imageClassificationFL/Client.py
@@ -13,13 +13,12 @@ from torch.utils.data import DataLoader
 import flwr as fl
 from flwr.common import Metrics
 from flwr_datasets import FederatedDataset
-"""
+
 DEVICE = torch.device("cpu")  # Try "cuda" to train on GPU
 print(
     f"Training on {DEVICE} using PyTorch {torch.__version__} and Flower {fl.__version__}"
 )
 disable_progress_bar()
-"""
 
 NUM_CLIENTS = 10
 BATCH_SIZE = 32
@@ -57,5 +56,98 @@ def load_datasets():
 
 trainloaders, valloaders, testloader = load_datasets()
 
+batch = next(iter(trainloaders[0]))
+images, labels = batch["img"], batch["label"]
+# Reshape and convert images to a NumPy array
+# matplotlib requires images with the shape (height, width, 3)
+images = images.permute(0, 2, 3, 1).numpy()
+# Denormalize
+images = images / 2 + 0.5
+
+# Create a figure and a grid of subplots
+fig, axs = plt.subplots(4, 8, figsize=(12, 6))
+
+# Loop over the images and plot them
+for i, ax in enumerate(axs.flat):
+    ax.imshow(images[i])
+    ax.set_title(trainloaders[0].dataset.features["label"].int2str([labels[i]])[0])
+    ax.axis("off")
+
+# Show the plot
+fig.tight_layout()
+plt.show()
 
 
+class Net(nn.Module):
+    def __init__(self) -> None:
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+def train(net, trainloader, epochs: int, verbose=False):
+    """Train the network on the training set."""
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters())
+    net.train()
+    for epoch in range(epochs):
+        correct, total, epoch_loss = 0, 0, 0.0
+        for batch in trainloader:
+            images, labels = batch["img"].to(DEVICE), batch["label"].to(DEVICE)
+            optimizer.zero_grad()
+            outputs = net(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            # Metrics
+            epoch_loss += loss
+            total += labels.size(0)
+            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+        epoch_loss /= len(trainloader.dataset)
+        epoch_acc = correct / total
+        if verbose:
+            print(f"Epoch {epoch + 1}: train loss {epoch_loss}, accuracy {epoch_acc}")
+
+
+def test(net, testloader):
+    """Evaluate the network on the entire test set."""
+    criterion = torch.nn.CrossEntropyLoss()
+    correct, total, loss = 0, 0, 0.0
+    net.eval()
+    with torch.no_grad():
+        for batch in testloader:
+            images, labels = batch["img"].to(DEVICE), batch["label"].to(DEVICE)
+            outputs = net(images)
+            loss += criterion(outputs, labels).item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    loss /= len(testloader.dataset)
+    accuracy = correct / total
+    return loss, accuracy
+
+
+trainloader = trainloaders[0]
+valloader = valloaders[0]
+net = Net().to(DEVICE)
+
+for epoch in range(5):
+    train(net, trainloader, 1)
+    loss, accuracy = test(net, valloader)
+    print(f"Epoch {epoch + 1}: validation loss {loss}, accuracy {accuracy}")
+
+loss, accuracy = test(net, testloader)
+print(f"Final test set performance:\n\tloss {loss}\n\taccuracy {accuracy}")
