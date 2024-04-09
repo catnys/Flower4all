4460b2e2799c5f1cee9615632a5a333f4521cdbf
aintburak <aintburak@gmail.com>
2024-04-09 11:09:51 +0300

    main: clean Server and Client from scratch

--- imageClassificationFL/Client.py
+++ imageClassificationFL/Client.py
@@ -1,27 +1,21 @@
+from collections import OrderedDict
+from typing import List, Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from datasets.utils.logging import disable_progress_bar
+from torch.utils.data import DataLoader
+
 import flwr as fl
-import tensorflow as tf
+from flwr.common import Metrics
+from flwr_datasets import FederatedDataset
 
-# Load model and data (MobileNetV2, CIFAR-10)
-model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
-model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-
-
-# Define Flower client
-class CifarClient(fl.client.NumPyClient):
-    def get_parameters(self, config):
-        return model.get_weights()
-
-    def fit(self, parameters, config):
-        model.set_weights(parameters)
-        model.fit(x_train, y_train, epochs=1, batch_size=32)
-        return model.get_weights(), len(x_train), {}
-
-    def evaluate(self, parameters, config):
-        model.set_weights(parameters)
-        loss, accuracy = model.evaluate(x_test, y_test)
-        return loss, len(x_test), {"accuracy": accuracy}
-
-
-# Start Flower client
-fl.client.start_numpy_client(server_address="127.0.0.1:8080", client=CifarClient())
+DEVICE = torch.device("cpu")  # Try "cuda" to train on GPU
+print(
+    f"Training on {DEVICE} using PyTorch {torch.__version__} and Flower {fl.__version__}"
+)
+disable_progress_bar()
