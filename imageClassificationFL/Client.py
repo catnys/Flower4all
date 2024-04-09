88b490e4588c13bcd85b2eab3f5e830d3b0ea5c9
aintburak <aintburak@gmail.com>
2024-04-09 18:23:16 +0300

    main: Update Client.py

--- imageClassificationFL/Client.py
+++ imageClassificationFL/Client.py
@@ -1,170 +1,25 @@
-from collections import OrderedDict
-from typing import List, Tuple
-
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
-
 import flwr as fl
-from flwr.common import Metrics
-from flwr_datasets import FederatedDataset
+import tensorflow as tf
 
-DEVICE = torch.device("cpu")  # Try "cuda" to train on GPU
-print(
-    f"Training on {DEVICE} using PyTorch {torch.__version__} and Flower {fl.__version__}"
-)
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
-NUM_CLIENTS = 10
-BATCH_SIZE = 32
+model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
+model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
 
 
-def load_datasets():
-    from flwr_datasets import FederatedDataset
-
-    fds = FederatedDataset(dataset="cifar10", partitioners={"train": NUM_CLIENTS})
-
-    def apply_transforms(batch):
-        transform = transforms.Compose(
-            [
-                transforms.ToTensor(),
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-            ]
-        )
-        batch["img"] = [transform(img) for img in batch["img"]]
-        return batch
-
-    trainloaders = []
-    valloaders = []
-    for partition_id in range(NUM_CLIENTS):
-        partition = fds.load_partition(partition_id, "train")
-        partition = partition.with_transform(apply_transforms)
-        partition = partition.train_test_split(train_size=0.8, seed=42)
-        trainloaders.append(DataLoader(partition["train"], batch_size=BATCH_SIZE))
-        valloaders.append(DataLoader(partition["test"], batch_size=BATCH_SIZE))
-    testset = fds.load_split("test").with_transform(apply_transforms)
-    testloader = DataLoader(testset, batch_size=BATCH_SIZE)
-    return trainloaders, valloaders, testloader
-
-
-trainloaders, valloaders, testloader = load_datasets()
-
-
-class Net(nn.Module):
-    def __init__(self) -> None:
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-
-def train(net, trainloader, epochs: int, verbose=False):
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(net.parameters())
-    net.train()
-    for epoch in range(epochs):
-        correct, total, epoch_loss = 0, 0, 0.0
-        for batch in trainloader:
-            images, labels = batch["img"].to(DEVICE), batch["label"].to(DEVICE)
-            optimizer.zero_grad()
-            outputs = net(images)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            # Metrics
-            epoch_loss += loss
-            total += labels.size(0)
-            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
-        epoch_loss /= len(trainloader.dataset)
-        epoch_acc = correct / total
-        if verbose:
-            print(f"Epoch {epoch + 1}: train loss {epoch_loss}, accuracy {epoch_acc}")
-
-
-def test(net, testloader):
-    criterion = torch.nn.CrossEntropyLoss()
-    correct, total, loss = 0, 0, 0.0
-    net.eval()
-    with torch.no_grad():
-        for batch in testloader:
-            images, labels = batch["img"].to(DEVICE), batch["label"].to(DEVICE)
-            outputs = net(images)
-            loss += criterion(outputs, labels).item()
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    loss /= len(testloader.dataset)
-    accuracy = correct / total
-    return loss, accuracy
-
-
-trainloader = trainloaders[0]
-valloader = valloaders[0]
-net = Net().to(DEVICE)
-
-for epoch in range(5):
-    train(net, trainloader, 1)
-    loss, accuracy = test(net, valloader)
-    print(f"Epoch {epoch + 1}: validation loss {loss}, accuracy {accuracy}")
-
-loss, accuracy = test(net, testloader)
-print(f"Final test set performance:\n\tloss {loss}\n\taccuracy {accuracy}")
-
-
-def set_parameters(net, parameters: List[np.ndarray]):
-    params_dict = zip(net.state_dict().keys(), parameters)
-    state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
-    net.load_state_dict(state_dict, strict=True)
-
-
-def get_parameters(net) -> List[np.ndarray]:
-    return [val.cpu().numpy() for _, val in net.state_dict().items()]
-
-
-class FlowerClient(fl.client.NumPyClient):
-    def __init__(self, net, trainloader, valloader):
-        self.net = net
-        self.trainloader = trainloader
-        self.valloader = valloader
-
+class CifarClient(fl.client.NumPyClient):
     def get_parameters(self, config):
-        return get_parameters(self.net)
+        return model.get_weights()
 
     def fit(self, parameters, config):
-        set_parameters(self.net, parameters)
-        train(self.net, self.trainloader, epochs=1)
-        return get_parameters(self.net), len(self.trainloader), {}
+        model.set_weights(parameters)
+        model.fit(x_train, y_train, epochs=1, batch_size=32, steps_per_epoch=3)
+        return model.get_weights(), len(x_train), {}
 
     def evaluate(self, parameters, config):
-        set_parameters(self.net, parameters)
-        loss, accuracy = test(self.net, self.valloader)
-        return float(loss), len(self.valloader), {"accuracy": float(accuracy)}
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(x_test, y_test)
+        return loss, len(x_test), {"accuracy": float(accuracy)}
 
 
-# Function to create a Flower client on demand
-def client_fn(cid: str) -> FlowerClient:
-    trainloader = trainloaders[int(cid)]
-    valloader = valloaders[int(cid)]
-    return FlowerClient(trainloader, valloader)
-
-
-# Entry point for running the client
-if __name__ == "__main__":
-    # Start Flower client
-    fl.client.start_numpy_client("0.0.0.0:8080", client=client_fn)
-
-
+fl.client.start_client(server_address="[::]:8080", client=CifarClient().to_client())
